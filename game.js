dd10a62c5db617db06c73cfe0a45070c71398654
Nicholas DeCicco <nsd.cicco@gmail.com>
2017-02-05 01:58:55 -0500

    Update speech bubble code (and other fixes)

--- game.js
+++ game.js
@@ -1,10 +1,102 @@
 
+var gc; /* GameController instance */
+
+window.onload = function() {
+    gc = new GameController();
+};
+
+function MsgBox() {
+    //show menu
+    
+    var curButton = 0;
+    var curMenu = 0; //0:"topLevelMenu", 1:"fight", 2:"act", 3:"git", 4:"mercy"
+    this.onkeydown=function(e) {
+    };
+    
+    //show diag
+    
+    // throwing an error for some reason
+    //menuObj = $("#menu");
+    
+    
+}
+
+function ActionArea() {
+	var actionArea = document.getElementById("actionArea");
+
+    /* Create one speech bubble and keep it hidden until we need it. */
+    var speechBubble = document.createElement("table");
+	speechBubble.setAttribute("class", "speechBubble");
+	//speechBubble.setAttribute("cellspacing", "0");
+	speechBubble.setAttribute("cellpadding", "0");
+
+        /* Top row */
+        var row = document.createElement("tr");
+            var cell = document.createElement("td");
+            cell.setAttribute("class", "speechBubble-top-left");
+            row.appendChild(cell);
+            
+            cell = document.createElement("td");
+            cell.setAttribute("class", "speechBubble-top");
+            row.appendChild(cell);
+            
+            cell = document.createElement("td");
+            cell.setAttribute("class", "speechBubble-top-right");
+            row.appendChild(cell);
+        speechBubble.appendChild(row);
+
+        /* Middle row */
+        row = document.createElement("tr");
+            cell = document.createElement("td");
+            cell.setAttribute("class", "speechBubble-middle-left");
+            row.appendChild(cell);
+            
+            var contentArea = document.createElement("td");
+            contentArea.setAttribute("class", "speechBubble-middle");
+            row.appendChild(contentArea);
+            
+            cell = document.createElement("td");
+            cell.setAttribute("class", "speechBubble-middle-right");
+            row.appendChild(cell);
+        speechBubble.appendChild(row);
+
+        /* Bottom row */
+        row = document.createElement("tr");
+            cell = document.createElement("td");
+            cell.setAttribute("class", "speechBubble-bottom-left");
+            row.appendChild(cell);
+            
+            cell = document.createElement("td");
+            cell.setAttribute("class", "speechBubble-bottom");
+            row.appendChild(cell);
+            
+            cell = document.createElement("td");
+            cell.setAttribute("class", "speechBubble-bottom-right");
+            row.appendChild(cell);
+        speechBubble.appendChild(row);
+
+    this.showSpeechBubble = function(text) {
+        speechBubble.style.visibility = "visible";
+        contentArea.innerText = text; // TODO: animate text
+        // FIXME: also have to set display: table?
+    };
+    
+    this.hideSpeechBubble = function() {
+        speechBubble.style.visibility = "hidden";
+        // also have to set display: none?
+    };
+
+	actionArea.appendChild(speechBubble);
+
+	this.hideSpeechBubble();
+}
+
 function GameController() {
     /*
      * UI elements
      */
-    var msgBox;
-    var actionArea;
+    var msgBox = new MsgBox();
+    var actionArea = new ActionArea();
     
     /*
      * Game state
@@ -80,106 +172,10 @@ function GameController() {
     
     // 2. Initialize view
     // Need to answer the question(s): what are the view states?
+
+	// Test: show speech bubble
+	actionArea.showSpeechBubble("foobar");
 }
-
-var gc; /* GameController instance */
-
-window.onload = function() {
-    gc = new GameController();
-};
-
-function MsgBox() {
-    //show menu
-    
-    var curButton = 0;
-    var curMenu = 0; //0:"topLevelMenu", 1:"fight", 2:"act", 3:"git", 4:"mercy"
-    this.onkeydown=function(e) {
-        if (/*toggle through buttons*/curMenu === 0) {
-            if (e.key == "ArrowRight") {
-               curButton = (curButton + 1)%4;
-            } else if (e.key == "ArrowLeft") {
-                if(curButton === 0){
-                    curButton = 3;
-                } else {
-                    curButton--;
-                }
-            } else if (e.key == "Enter") {
-
-                }
-            }
-        } else {
-            
-        }
-    }
-    
-    //show diag
-    
-    menuObj = $("#menu");
-    
-    
-}
-
-function ActionArea() {
-    /* Create one speech bubble and keep it hidden until we need it. */
-    var speechBubble = document.createElement("table");
-
-        /* Top row */
-        var row = document.createElement("tr");
-            var cell = document.createElement("td");
-            cell.setAttribute("class", "speechBubble-top-left");
-            row.appendChild(cell);
-            
-            cell = document.createElement("td");
-            cell.setAttribute("class", "speechBubble-top");
-            row.appendChild(cell);
-            
-            cell = document.createElement("td");
-            cell.setAttribute("class", "speechBubble-top-right");
-            row.appendChild(cell);
-        speechBubble.appendChild(row);
-
-        /* Middle row */
-        row = document.createElement("tr");
-            cell = document.createElement("td");
-            cell.setAttribute("class", "speechBubble-middle-left");
-            row.appendChild(cell);
-            
-            cell = document.createElement("td");
-            cell.setAttribute("class", "speechBubble-middle");
-            row.appendChild(cell);
-            
-            cell = document.createElement("td");
-            cell.setAttribute("class", "speechBubble-middle-right");
-            row.appendChild(cell);
-        speechBubble.appendChild(row);
-
-        /* Bottom row */
-        row = document.createElement("tr");
-            cell = document.createElement("td");
-            cell.setAttribute("class", "speechBubble-bottom-left");
-            row.appendChild(cell);
-            
-            cell = document.createElement("td");
-            cell.setAttribute("class", "speechBubble-bottom");
-            row.appendChild(cell);
-            
-            cell = document.createElement("td");
-            cell.setAttribute("class", "speechBubble-bottom-right");
-            row.appendChild(cell);
-        speechBubble.appendChild(row);
-    
-    this.showSpeechBubble = function(text) {
-        speechBubble.style.visibility = "visible";
-        speechBubble.innerText = text; // TODO: animate text
-        // FIXME: also have to set display: table?
-    };
-    
-    this.hideSpeechBubble = function() {
-        speechBubble.style.visibility = "hidden";
-        // also have to set display: none?
-    };
-}
-
 
 
 
